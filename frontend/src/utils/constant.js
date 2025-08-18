47c3e31f16b8a603af55a8649c9f7d88ac678cf0
princeshukla13 <princeshukla8500@gmail.com>
2025-08-18 18:08:33 +0530

    rendor link

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,4 +1,9 @@
-export const USER_API_END_POINT="http://localhost:8000/api/v1/user";
-export const JOB_API_END_POINT="http://localhost:8000/api/v1/job";
-export const APPLICATION_API_END_POINT="http://localhost:8000/api/v1/application";
-export const COMPANY_API_END_POINT="http://localhost:8000/api/v1/company";
+const BASE_URL =
+  import.meta.env.MODE === "development"
+    ? "http://localhost:8000" // local backend
+    : "https://jobprotalbackend-sraz.onrender.com"; // Render backend
+
+export const USER_API_END_POINT = `${BASE_URL}/api/v1/user`;
+export const JOB_API_END_POINT = `${BASE_URL}/api/v1/job`;
+export const APPLICATION_API_END_POINT = `${BASE_URL}/api/v1/application`;
+export const COMPANY_API_END_POINT = `${BASE_URL}/api/v1/company`;
